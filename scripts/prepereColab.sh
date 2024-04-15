fa1000dd7344185d9e95eebc16c512abf5918ea1
IgorCzudy <iczudy@gmail.com>
2024-04-15 15:56:34 +0200

    feat: preperColab script quaiet

--- scripts/prepereColab.sh
+++ scripts/prepereColab.sh
@@ -1,4 +1,3 @@
-pip install mlflow --quiet
-pip install dvc --quiet
+pip install torch mlflow dvc datasets accelerate -U transformers[torch] --quiet
 dvc remote add --default master_thesis /content/drive/MyDrive/master_thesis
 dvc pull
