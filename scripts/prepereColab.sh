f44f2027865acda69776ab0678d2c885989d3f2f
IgorCzudy <igor.czudy@student.put.poznan.pl>
2024-04-21 13:03:27 +0200

    fear: sacremoses install

--- scripts/prepereColab.sh
+++ scripts/prepereColab.sh
@@ -1,3 +1,3 @@
-pip install torch mlflow dvc datasets accelerate -U transformers[torch] --quiet
+pip install torch sacremoses mlflow dvc datasets accelerate -U transformers[torch] --quiet
 dvc remote add --default master_thesis /content/drive/MyDrive/master_thesis
 dvc pull
