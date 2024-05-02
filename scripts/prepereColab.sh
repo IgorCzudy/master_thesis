2c03bb0f70b00062c1c158393f5f89028a2bacf9
IgorCzudy <iczudy@gmail.com>
2024-05-02 11:46:19 +0200

    fix: torch version

--- scripts/prepereColab.sh
+++ scripts/prepereColab.sh
@@ -1,3 +1,3 @@
-pip install torch sacremoses mlflow dvc datasets accelerate -U transformers[torch] --quiet
+pip install torch==2.2.1 sacremoses mlflow dvc datasets accelerate -U transformers[torch] --quiet
 dvc remote add --default master_thesis /content/drive/MyDrive/master_thesis
 dvc pull
