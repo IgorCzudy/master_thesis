03660394cc0b22f7e5c62060f5fb6463a0767d79
IgorCzudy <igor.czudy@student.put.poznan.pl>
2024-06-28 17:15:57 +0200

    feat: both language read

--- train/tokenization.py
+++ train/tokenization.py
@@ -6,6 +6,7 @@ from transformers import AutoTokenizer
 
 def read_data(
     for_english,
+    for_both,
     path="/content/master_thesis/data/translated_go_emotion.csv",
     proc_of_ds=1,
 ):
@@ -22,6 +23,27 @@ def read_data(
         "rater_id",
         "example_very_unclear",
     ]
+    if for_both:
+        col_to_del_set = set(col_to_del) | set(["text_pl", "text"])
+        labels = set(df.columns.tolist()) - col_to_del_set
+
+
+        df = pd.concat([
+            df[list(set(['text']) | labels)].rename(columns={'text': 'text'}),
+            df[list(set(['text_pl']) | labels)].rename(columns={'text_pl': 'text'})
+        ])
+        # df.drop(columns=col_to_del, inplace=True)
+
+        df.reset_index(drop=True, inplace=True)
+        df = df[['text'] + list(labels)]
+        df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+        df = df.replace({0: False, 1: True})
+        labels = df.columns.tolist()[1:]
+
+        size = int(df.shape[0] * proc_of_ds)
+        random_rows = df.sample(n=size, replace=False)
+        return random_rows, labels
+
     if for_english:
         col_to_del = ["text_pl"] + col_to_del
         df.drop(columns=col_to_del, inplace=True)
@@ -73,3 +95,11 @@ def preprocess_data(examples, tokenizer, labels):
         labels_matrix[:, idx] = labels_batch[label]
     encoding["labels"] = labels_matrix.tolist()
     return encoding
+
+
+read_data(
+    for_english = False,
+    for_both= True,
+    path="data/translated_go_emotion.csv",
+    proc_of_ds=1,
+)
